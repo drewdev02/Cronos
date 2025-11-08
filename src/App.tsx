2c8ad9109ab0e77f9e93040f6594cc2ba255cdb9
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2025-11-07 19:58:21 -0500

    feat: Implement routing for Timer and Customer modules in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,32 @@ import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "next-themes";
 import { useTimerTrayIntegration } from "@/modules/Timer/hooks/use-timer-tray-integration.ts";
 import { Customer } from "./modules/Customer";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+
+enum AppRoute {
+  Timer = "/timer",
+  Customer = "/customer",
+}
+
+type Route = {
+  path: AppRoute;
+  label: string;
+  element: JSX.Element;
+};
+
+const routes: Route[] = [
+  {
+    path: AppRoute.Timer,
+    label: "Timer",
+    element: <Timer />,
+  },
+  {
+    path: AppRoute.Customer,
+    label: "Customer",
+    element: <Customer />,
+  },
+];
 
 export default function App() {
   // Initialize tray integration
@@ -10,8 +36,16 @@ export default function App() {
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <Customer />
-      <Toaster />
+      <HashRouter>
+        {/* Navegación eliminada por solicitud */}
+        <Routes>
+          {routes.map(route => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+          <Route path="*" element={<Timer />} />
+        </Routes>
+        <Toaster />
+      </HashRouter>
     </ThemeProvider>
   )
 }
