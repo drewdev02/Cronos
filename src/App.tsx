023b918aa49a07fd81c3010cfc046a3dd53e198e
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2025-11-07 20:12:04 -0500

    feat: Integrate Sidebar component and refactor routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { ThemeProvider } from "next-themes";
 import { useTimerTrayIntegration } from "@/modules/Timer/hooks/use-timer-tray-integration.ts";
 import { Customer } from "./modules/Customer";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarRail } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 
 
 enum AppRoute {
@@ -11,7 +14,7 @@ enum AppRoute {
   Customer = "/customer",
 }
 
-type Route = {
+export type Route = {
   path: AppRoute;
   label: string;
   element: JSX.Element;
@@ -37,14 +40,18 @@ export default function App() {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <HashRouter>
-        {/* Navegación eliminada por solicitud */}
-        <Routes>
-          {routes.map(route => (
-            <Route key={route.path} path={route.path} element={route.element} />
-          ))}
-          <Route path="*" element={<Timer />} />
-        </Routes>
-        <Toaster />
+        <SidebarProvider>
+          <AppSidebar routes={routes} />
+          <div className="ml-[16rem]">
+            <Routes>
+              {routes.map(route => (
+                <Route key={route.path} path={route.path} element={route.element} />
+              ))}
+              <Route path="*" element={<Timer />} />
+            </Routes>
+            <Toaster />
+          </div>
+        </SidebarProvider>
       </HashRouter>
     </ThemeProvider>
   )
